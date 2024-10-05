7427a7f759dbb7e781edddae50b5153892558f28
Makima3 <natalia.kolchuk@gmail.com>
2024-10-05 21:09:46 +0300

    add management

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'users.UserCustomModel'
@@ -33,11 +33,11 @@ AUTH_USER_MODEL = 'users.UserCustomModel'
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.staticfiles",
-    "django_countries",
-    "django_filters",
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.staticfiles',
+    'django_countries',
+    'django_filters',
     # my apps
     'apps.users',
     'apps.car',
--- backend/core/management/commands/wait_db.py
+++ backend/core/management/commands/wait_db.py
@@ -1,10 +1,10 @@
 import time
 
-from django.core.management.base import BaseCommand
+from django.core.management import BaseCommand
 from django.db import OperationalError, connection
 from django.db.backends.postgresql.base import DatabaseWrapper
 
-connection: DatabaseWrapper = connection
+connection:DatabaseWrapper = connection
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -14,10 +14,9 @@ class Command(BaseCommand):
         while not con_db:
             try:
                 connection.ensure_connection()
-                con_db = True
+                con_db =True
             except OperationalError:
-                self.stdout.write('Database unavailable,wait 3 seconds...')
+                self.stdout.write('Database unavailable, wait 3 seconds...')
                 time.sleep(3)
 
-        self.stdout.write('Database available!')
-
+        self.stdout.write(self.style.SUCCESS('Database available!'))
