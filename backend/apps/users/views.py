a377d71d406389cb5dfc4ff76d92a6c01400bea9
Makima3 <natalia.kolchuk@gmail.com>
2024-10-13 23:55:50 +0300

    create adv

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from apps.users.serializers import UserSerializer
 
@@ -9,5 +10,6 @@ UserModel = get_user_model()
 class UserListCreateView(ListCreateAPIView):
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 
